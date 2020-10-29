8731e527379123cfe6e1f931df16e8452018a34e
Mingsicogi <codedoctor119@gmail.com>
2020-10-29 16:13:57 +0900

    + generic example code

---
+++ src/main/java/mins/study/board/app/Node.java
@@ -0,0 +1,12 @@
+package mins.study.board.app;
+
+public class Node {
+
+    Node link;
+    int value;
+
+    public Node(Node link, int value) {
+        this.link = link;
+        this.value = value;
+    }
+}
