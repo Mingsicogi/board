c66dbc931eb9e90285b94333aa6943afd28275ac
Mingsicogi <codedoctor119@gmail.com>
2020-10-31 16:14:50 +0900

    + generic example code

---
+++ src/test/java/mins/study/board/StringEqualsTest.java
@@ -0,0 +1,16 @@
+package mins.study.board;
+
+import org.junit.jupiter.api.Test;
+
+public class StringEqualsTest {
+
+    @Test
+    void test() {
+        String temp = null;
+
+//        Object
+
+//        System.out.println(temp.equals(""));
+        System.out.println("".equals(temp));
+    }
+}
